a5a700c9622d16354daeca9bfe34516855e1296d
Ahmed Dobban <73547094+AhmedDoban@users.noreply.github.com>
2023-06-10 22:03:41 +0300

    update login

--- src/assets/Screens/Login/Login.jsx
+++ src/assets/Screens/Login/Login.jsx
@@ -8,8 +8,35 @@ function Login(props) {
   return (
     <React.Fragment>
       <div className="login">
-        <h1>Login page</h1>
-        <button onClick={() => HandelLogin()}> a;lkda</button>
+        <div className="left">
+          <div className="logo">
+            <img src={require("../../img/logo.svg").default} alt="" />
+          </div>
+          <h1>Quote, Book & Track Your Cargo Online</h1>
+          <p>
+            Manage all of your freight requirements in one place. Simply log in
+            to experience the benefits of digital freight forwarding and ship
+            with ease.
+          </p>
+        </div>
+        <div className="right">
+          <div className="data">
+            <h3>Welcome to iContainers</h3>
+            <p>
+              Don’t have an account? <a href="">Register Here</a>
+            </p>
+          </div>
+          <form action="">
+            <input type="email" name="" id="" placeholder="Work Email *" />
+            <input type="password" name="" id="" placeholder="password *" />
+            <div className="remember">
+              <input type="checkbox" name="" id="remember" />
+              <label htmlFor="remember">Remember Me</label>
+            </div>
+            <button onClick={() => HandelLogin()}>Login</button>
+            <a href="">Forgot Password?</a>
+          </form>
+        </div>
       </div>
     </React.Fragment>
   );
